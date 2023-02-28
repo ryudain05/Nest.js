58be51d7d17248e2f277e5b1a53e39d5dc39a7be
RDI <dkdlsekdls@naver.com>
2023-02-28 15:18:40 +0900

    feat/Movies Service part Two

--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -6,7 +6,6 @@ import {
   Delete,
   Patch,
   Body,
-  Query,
 } from '@nestjs/common';
 import { MoviesService } from './movies.service';
 import { Movie } from './entities/movie.entity';
@@ -37,9 +36,6 @@ export class MoviesController {
 
   @Patch('/:id')
   path(@Param('id') movieId: string, @Body() updateData) {
-    return {
-      updateMovie: movieId,
-      ...updateData,
-    };
+    return this.MoviesService.update(movieId, updateData);
   }
 }
